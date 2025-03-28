a0047e764b9afe01b2833ada43619ed1cea316f3
DigitalDreamsKC <mary@digitaldreamskc.com>
2025-03-27 20:03:48 -0500

    Fix hydration issues and update page component

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,8 @@
 const nextConfig = {
     reactStrictMode: true,
     env: {
-        NEXT_PUBLIC_THIRDWEB_CLIENT_ID: process.env.THIRDWEB_CLIENT_ID,
+        NEXT_PUBLIC_THIRDWEB_CLIENT_ID: process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID,
     },
 };
-
 export default nextConfig;
 
